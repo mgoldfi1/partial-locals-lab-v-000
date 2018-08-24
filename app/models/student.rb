6bb6e41a0feb178656b6b21f176d5e1fd2cdec15
Matthew Goldfine <mgoldfi1@binghamton.edu>
2018-08-24 15:52:23 +0000

    automatically backed up by learn

--- app/models/student.rb
+++ app/models/student.rb
@@ -13,4 +13,15 @@
 class Student < ActiveRecord::Base
   has_many :classroom_students
   has_many :classrooms, through: :classroom_students
+
+
+
+  def self.search(name)
+    if name != ""
+    Student.find_by(name: name)
+  else
+    Student.all
+  end
+end
+
 end
